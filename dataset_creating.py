a0a3202982a511a0dcd61a109a188aaf808538a1
wrongserenity <43683367+wrongserenity@users.noreply.github.com>
2020-11-19 15:14:59 +0300

    Create dataset_creating.py

---
+++ dataset_creating.py
@@ -0,0 +1,10 @@
+# sry 4 ma English
+#
+# TODO:
+# find any big data of text without mistakes
+# using random change some of words with database prepared by Kostya
+# use embeddings
+# try N-grams model or attention system
+
+# ask Davydenko about research article
+
